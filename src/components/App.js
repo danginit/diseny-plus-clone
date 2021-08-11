9ae8ea8d82b7bc5927083c5c2975bb32e0b4a684
rorong <rordev@ongraph.com>
2021-08-11 12:49:57 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Login from "./Login";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
